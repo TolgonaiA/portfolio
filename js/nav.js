7e5f95b962b38a4dc00b2bbf1457ebf79e3b0912
Tolgonai Andakulova <tolgonaia@gmail.com>
2023-02-20 20:04:00 +0600

    added ic to skills

--- js/nav.js
+++ js/nav.js
@@ -1,14 +1,14 @@
-function navToggle() {
-    let navBtn = document.getElementById('navBtn');
-    let aside = document.getElementById('aside');
-  
-    navBtn.onclick = function () {
-      if (aside.classList.contains('nav-hidden')) {
-        aside.classList.remove('nav-hidden');
-      } else {
-        aside.classList.add('nav-hidden');
-      }
-    }
-}
-  
-navToggle();
+// function navToggle() {
+//     let navBtn = document.getElementById('navBtn');
+//     let aside = document.getElementById('aside');
+//
+//     navBtn.onclick = function () {
+//       if (aside.classList.contains('nav-hidden')) {
+//         aside.classList.remove('nav-hidden');
+//       } else {
+//         aside.classList.add('nav-hidden');
+//       }
+//     }
+// }
+//
+// navToggle();
