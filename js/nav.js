adb4e2ea0558d3387d4a9fe70a37b6aaca711efb
Tolgonai Andakulova <tolgonaia@gmail.com>
2023-02-22 11:50:20 +0600

    added responsive styles

--- js/nav.js
+++ js/nav.js
@@ -17,32 +17,32 @@
 const btn = document.getElementById("navBtn");
 const nav = document.getElementById("nav");
 
-if (window.innerWidth < 901) {
-  btn.classList.add('header-btn');
-  nav.classList.add('aside-nav-media');
-}
-
-
-const reportWindowSize = () => {
-  if (window.innerWidth < 901) {
-    btn.classList.add('btn-media');
-    nav.classList.add('main-nav-media');
-    // socials.classList.add('socials-media');
-  } else if (window.innerWidth >= 901) {
-    btn.classList.remove('btn-media');
-    nav.classList.remove('main-nav-media');
-    // socials.classList.remove('socials-media');
-  }
-}
-
-window.onresize = reportWindowSize;
-
-window.addEventListener("resize", reportWindowSize);
-
-btn.addEventListener('click', () => {
-  if (nav.classList.contains('main-nav-media-show')) {
-    nav.classList.remove('main-nav-media-show')
-  } else {
-    nav.classList.add('main-nav-media-show')
-  }
-});
+// if (window.innerWidth < 901) {
+//   btn.classList.add('header-btn');
+//   nav.classList.add('aside-nav-media');
+// }
+//
+//
+// const reportWindowSize = () => {
+//   if (window.innerWidth < 901) {
+//     btn.classList.add('btn-media');
+//     nav.classList.add('main-nav-media');
+//     // socials.classList.add('socials-media');
+//   } else if (window.innerWidth >= 901) {
+//     btn.classList.remove('btn-media');
+//     nav.classList.remove('main-nav-media');
+//     // socials.classList.remove('socials-media');
+//   }
+// }
+//
+// window.onresize = reportWindowSize;
+//
+// window.addEventListener("resize", reportWindowSize);
+//
+// btn.addEventListener('click', () => {
+//   if (nav.classList.contains('main-nav-media-show')) {
+//     nav.classList.remove('main-nav-media-show')
+//   } else {
+//     nav.classList.add('main-nav-media-show')
+//   }
+// });
