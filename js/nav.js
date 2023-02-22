80ae5b6fdc9f19a37841259f67fa4f7dab11667c
Tolgonai Andakulova <tolgonaia@gmail.com>
2023-02-22 12:25:09 +0600

    fix bug

--- js/nav.js
+++ js/nav.js
@@ -1,18 +1,3 @@
-// function navToggle() {
-//     let navBtn = document.getElementById('navBtn');
-//     let aside = document.getElementById('aside');
-//
-//     navBtn.onclick = function () {
-//       if (aside.classList.contains('nav-hidden')) {
-//         aside.classList.remove('nav-hidden');
-//       } else {
-//         aside.classList.add('nav-hidden');
-//       }
-//     }
-// }
-//
-// navToggle();
-
 
 const btn = document.getElementById("navBtn");
 const nav = document.getElementById("nav");
